ab827803792b60bcfb66ccbe5d5bd1cd3d52c32e
CainanH1012 <cainanh101@gmail.com>
2024-07-14 10:24:17 -0400

    Delete tests directory

--- tests/test_routes.py
+++
@@ -1,8 +0,0 @@
-def test_index(client):
-    rv = client.get('/')
-    assert rv.status_code == 200
-    assert b'PGN: Underground CAD System' in rv.data
-
-def test_login(client):
-    rv = client.get('/login/')
-    assert rv.status_code == 302  # Redirect to Discord login
